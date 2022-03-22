a91f744d3368a5aba1a626ec1ece8b691469c9c0
SergeyD <drobilenko94@mail.ru>
2022-03-22 16:23:14 +0300

    first commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "94.130.167.163", // Адрес FTP сервера
+    user: "drobi859", // Имя пользователя
+    password: "oVuA54PqZI", // Пароль
+    parallel: 5 // Кол-во одновременных потоков
+}
